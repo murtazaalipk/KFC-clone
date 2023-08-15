f5319a7c6967ccfbfb51d2d6ea80f5fcadd43398
Syed Murtaza Ali <125219980+SyedMurtazaAli-dev@users.noreply.github.com>
2023-08-16 00:21:44 +0500

    button update

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "1ef7120d9b44c102"; }
+/******/ 		__webpack_require__.h = function() { return "f9800140c62029bb"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/components/header.js
+++ src/components/header.js
@@ -2,7 +2,7 @@ import Nav from '@/components/nav'
 export default function Header(){
     return(
         
-        <div className='bg-black text-white'>
+        <div className='bg-black text-white font-sans font-bold '>
             <Nav/>
          <div className='flex justify-center items-center gap-4 p-4 pb-8' >
             <a className='px-3 py-3 bg-[#1c1816] rounded-[8px] cursor-pointer' >EveryDay Value</a>
